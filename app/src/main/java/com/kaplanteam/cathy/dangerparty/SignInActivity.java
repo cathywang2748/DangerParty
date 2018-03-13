5abd99be0b2c0c8f05f81901608f74168d5a6c55
Cathy <cathywang2748@yahoo.com>
2018-03-13 13:56:16 -0700

    started sign-in activity

--- app/src/main/java/com/kaplanteam/cathy/dangerparty/SignInActivity.java
+++ app/src/main/java/com/kaplanteam/cathy/dangerparty/SignInActivity.java
@@ -1,14 +1,25 @@
 package com.kaplanteam.cathy.dangerparty;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private SignInButton signInButton;
+    public static final int RC_SIGN_IN = 1;
+    private GoogleSignInClient c;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +31,17 @@ public class SignInActivity extends AppCompatActivity {
                 .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
+        signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(this);
         // Build a GoogleSignInClient with the options specified by gso.
-        GoogleSignInClient c = GoogleSignIn.getClient(this, gso);
+        c = GoogleSignIn.getClient(this, gso);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         // Check for existing Google Sign In account, if the user is already signed in
-// the GoogleSignInAccount will be non-null.
+        // the GoogleSignInAccount will be non-null.
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
         updateUI(account);
 
@@ -38,5 +51,46 @@ public class SignInActivity extends AppCompatActivity {
         //hide the sign-in button,
         // launch your main activity,
         // or whatever is appropriate for your app.
+    }
+
+    private void signIn() {
+        Intent signInIntent = c.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.d("TAG", "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
     }
 }
