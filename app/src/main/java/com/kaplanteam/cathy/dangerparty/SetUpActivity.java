5ff1f733d1415e5529bfc330073bd2c459216363
Cathy <cathywang2748@yahoo.com>
2018-03-15 13:19:12 -0700

    more sign-in activity added SetUpActivity and the layout for it

---
+++ app/src/main/java/com/kaplanteam/cathy/dangerparty/SetUpActivity.java
@@ -0,0 +1,15 @@
+package com.kaplanteam.cathy.dangerparty;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by per6 on 3/15/18.
+ */
+
+public class SetUpActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_up);
+    }
+}
