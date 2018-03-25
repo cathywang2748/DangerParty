952d80bf9c7e4075a769a96a12f821a4cf804796
Cole <coletschroeder@gmail.com>
2018-03-24 17:58:44 -0700

    map x moves

--- app/src/main/java/com/kaplanteam/cathy/dangerparty/GameActivity.java
+++ app/src/main/java/com/kaplanteam/cathy/dangerparty/GameActivity.java
@@ -5,10 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Cole on 3/22/18.
@@ -26,8 +27,10 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
         final ImageView needle = findViewById(R.id.imageView_compass_needle);
         angle = 0;
-        needle.setRotation((float)angle);
         needle.setOnTouchListener(this);
+
+        final ImageView xMark = findViewById(R.id.imageView_x);
+        xMark.setOnTouchListener(this);
 
         wireWidgets();
         setOnClickListeners();
@@ -54,7 +57,6 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         if(view.getId() == R.id.imageView_compass_needle){
             switch(action) {
                 case (MotionEvent.ACTION_DOWN) :
-                    Log.d("Motion Event: ","Action was DOWN");
                     return true;
                 case (MotionEvent.ACTION_MOVE) :
                     float ivposx =  (view.getBottom() - view.getTop()) /2 - 75;
@@ -74,16 +76,56 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
                 case (MotionEvent.ACTION_UP) :
                     angle = Math.round(angle/45)*45;
                     view.setRotation((float)angle);
-
-                    Log.d("Motion Event: ","Action was UP");
                     return true;
                 default :
                     return false;
             }
         }
         else if (view.getId() == R.id.imageView_x){
-
-            return true;
+            float x, y, currentX, currentY;
+            switch(action) {
+                case (MotionEvent.ACTION_DOWN):
+                    return true;
+                case (MotionEvent.ACTION_MOVE):
+                    x = motionEvent.getX();
+                    y = motionEvent.getY();
+                    currentX = view.getX();
+                    currentY = view.getY();
+                    view.setX(x + currentX - 32);
+                    view.setY(y + currentY - 32);
+                    return true;
+                case (MotionEvent.ACTION_UP):
+                    //snap to nearest 0 (71), 260, 496, 648
+                    x = motionEvent.getX();
+                    y = motionEvent.getY();
+                    currentX = view.getX();
+                    currentY = view.getY();
+                    ArrayList<Float> locations = new ArrayList<>();
+                    locations.add((float)71);
+                    locations.add((float)260);
+                    locations.add((float)496);
+                    locations.add((float)648);
+                    float minval = locations.get(0);
+                    int min = 0;
+                    for(int i = 1;i < locations.size();i++){
+                        if (Math.pow(minval-(x+currentX), 2) > Math.pow(locations.get(i)-(x+currentX), 2)){
+                            minval = locations.get(i);
+                            min = i;
+                        }
+                    }
+                    view.setX(minval);
+                    if(min == 0)
+                        view.setY(192);
+                    else if(min == 1)
+                        view.setY(114);
+                    else if(min == 2)
+                        view.setY(220);
+                    else
+                        view.setY(130);
+                    return true;
+                default:
+                    return false;
+            }
         }
         else
             return false;
