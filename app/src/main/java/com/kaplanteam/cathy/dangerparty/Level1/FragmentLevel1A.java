8ebec4cbae03d0137683c9b5ead596d6a85fd198
Cole <coletschroeder@gmail.com>
2018-03-29 13:06:28 -0700

    1a layout wired and stuff

--- app/src/main/java/com/kaplanteam/cathy/dangerparty/Level1/FragmentLevel1A.java
+++ app/src/main/java/com/kaplanteam/cathy/dangerparty/Level1/FragmentLevel1A.java
@@ -2,8 +2,11 @@ package com.kaplanteam.cathy.dangerparty.Level1;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MotionEventCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -23,6 +26,7 @@ public class FragmentLevel1A extends Fragment implements View.OnTouchListener, V
     private Button octopus, doggiePaddle, corkscrew, dolphin, elegantUndulations;
     private Switch drown, pressbutton;
     private ImageView bubbleS, bubbleM, bubbleL, vortex;
+    private double angle, initial, last, theta;
 
     @Nullable
     @Override
@@ -32,6 +36,10 @@ public class FragmentLevel1A extends Fragment implements View.OnTouchListener, V
 
         //wire any widgets -- must use rootView.findViewById
 
+        angle = 0;
+        initial = 0;
+        last = 0;
+        theta = 0;
         wireWidgets(rootView);
         setListeners();
 
@@ -72,16 +80,120 @@ public class FragmentLevel1A extends Fragment implements View.OnTouchListener, V
 
     @Override
     public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.imageView_bubble_small:
+                final ImageView vs = (ImageView) view;
+                vs.setImageDrawable(getResources().getDrawable(R.drawable.pop));
+                CountDownTimer timers = new CountDownTimer(4000, 2000) {
+                    @Override
+                    public void onTick(long l) {
+                        vs.setVisibility(View.INVISIBLE);
+                    }
 
+                    @Override
+                    public void onFinish() {
+                        vs.setImageDrawable(getResources().getDrawable(R.drawable.bubble));
+                        vs.setVisibility(View.VISIBLE);
+                    }
+                }.start();
+                break;
+            case R.id.imageView_bubble_medium:
+                final ImageView vm = (ImageView) view;
+                vm.setImageDrawable(getResources().getDrawable(R.drawable.pop));
+                CountDownTimer timerm = new CountDownTimer(4000, 2000) {
+                    @Override
+                    public void onTick(long l) {
+                        vm.setVisibility(View.INVISIBLE);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        vm.setImageDrawable(getResources().getDrawable(R.drawable.bubble));
+                        vm.setVisibility(View.VISIBLE);
+                    }
+                }.start();
+                break;
+            case R.id.imageView_bubble_large:
+                final ImageView vl = (ImageView) view;
+                vl.setImageDrawable(getResources().getDrawable(R.drawable.pop));
+                CountDownTimer timerl = new CountDownTimer(4000, 2000) {
+                    @Override
+                    public void onTick(long l) {
+                        vl.setVisibility(View.INVISIBLE);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        vl.setImageDrawable(getResources().getDrawable(R.drawable.bubble));
+                        vl.setVisibility(View.VISIBLE);
+                    }
+                }.start();
+                break;
+            case R.id.button_octopus:
+                break;
+            case R.id.button_doggie_paddle:
+                break;
+            case R.id.button_corkscrew:
+                break;
+            case R.id.button_dolphin:
+                break;
+            case R.id.button_elegant_undulations:
+                break;
+        }
     }
 
     @Override
     public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-
+        switch(compoundButton.getId()){
+            case R.id.switch_drown:
+                break;
+            case R.id.switch_pressbutton:
+                break;
+        }
     }
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-        return false;
+        int action = MotionEventCompat.getActionMasked(motionEvent);
+        switch(action) {
+            case (MotionEvent.ACTION_DOWN):
+                initial = angle;
+                return true;
+            case (MotionEvent.ACTION_MOVE):
+                float ivposx =  (view.getBottom() - view.getTop()) /2 - 75;
+                float ivposy = (view.getRight() - view.getLeft()) /2 + 80;
+                float me = motionEvent.getY();
+                float x = motionEvent.getX() - ivposx;
+                float y = ivposy - motionEvent.getY();
+                if(x!=0){
+                    theta = Math.atan(y/x);
+                    theta = theta * 180/Math.PI;
+                    if(x < 0){theta -= 180;}
+                    theta = 90 - theta;
+                    angle = theta + angle;
+                    view.setRotation((float)angle);
+                }
+                Log.d("angle: ", "" + angle);
+                if(angle < 180 + last){
+                    ImageView v = (ImageView) view;
+                    v.setImageDrawable(getResources().getDrawable(R.drawable.clockwise));
+                    Log.d("CLOCKWISE", " Fired");
+                }
+                else {
+                    ImageView v = (ImageView) view;
+                    v.setImageDrawable(getResources().getDrawable(R.drawable.counterclockwise));
+                    Log.d("COUNTERCLOCKWISE", " Fired");
+                }
+                last = angle;
+                return true;
+            case (MotionEvent.ACTION_UP):
+                if(angle - initial < 30 && angle - initial > -30 ){
+                    ImageView v = (ImageView) view;
+                    v.setImageDrawable(getResources().getDrawable(R.drawable.vortex));
+                }
+                return true;
+            default:
+                return false;
+        }
     }
 }
